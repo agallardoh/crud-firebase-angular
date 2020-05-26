2ad0bc3e05609a8de2b8628177e4460995d73a9e
Agustin Gallardo <agustingh1583@gmail.com>
2020-05-26 14:26:06 -0500

    Registro de heroe

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroeModel } from '../../models/heroe.model';
+import { NgForm } from '@angular/forms';
+import { IfStmt } from '@angular/compiler';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  heroe = new HeroeModel();
+
+  constructor( private heroesService: HeroesService) { }
 
   ngOnInit() {
   }
 
+  guardar( form: NgForm) {
+
+    if (form.invalid) {
+
+      console.log('Formulario no valido');
+      return;
+    }
+    this.heroesService.crearHeroe(this.heroe)
+    .subscribe( resp => {
+      console.log(resp);
+    });
+  }
 }
